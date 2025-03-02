320ba58d894e530ce7d884ec003ec24d6f8e6ee2
Charlie Jin <52726156+HBBReviews@users.noreply.github.com>
2025-03-02 16:10:32 -0500

    Implemented Tessellation Shaders

--- unity-wildfire-port/unity-wildfire-port/unity-wildfire-port/main.cpp
+++ unity-wildfire-port/unity-wildfire-port/unity-wildfire-port/main.cpp
@@ -7,7 +7,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-#include <learnopengl/shader_m.h>
+#include <learnopengl/shader_t.h>
 #include <learnopengl/camera.h>
 
 #include <iostream>
@@ -19,20 +19,15 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
 
-// Initial Settings
-const unsigned int SCR_WIDTH = 1280;
-const unsigned int SCR_HEIGHT = 720;
-int useWireframe = 0;
-int displayGrayscale = 0;
-
-glm::vec3 startingCameraPosition = glm::vec3(67.0f, 627.5f, 169.9f);
-glm::vec3 cameraUpVector = glm::vec3(0.0f, 1.f, 0.f);
-const float STARTING_CAMERA_YAW = -128.1f;
-const float STARTING_CAMERA_PITCH = -42.4f;
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+const unsigned int NUM_PATCH_PTS = 4;
 
 // camera - give pretty starting point
-Camera camera(startingCameraPosition, cameraUpVector, STARTING_CAMERA_YAW, STARTING_CAMERA_PITCH);
-
+Camera camera(glm::vec3(67.0f, 627.5f, 169.9f),
+    glm::vec3(0.0f, 1.0f, 0.0f),
+    -128.1f, -42.4f);
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -46,8 +41,8 @@ int main()
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
@@ -56,7 +51,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL: Terrain CPU", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL: Terrain GPU", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -80,85 +75,103 @@ int main()
         return -1;
     }
 
+    GLint maxTessLevel;
+    glGetIntegerv(GL_MAX_TESS_GEN_LEVEL, &maxTessLevel);
+    std::cout << "Max available tess level: " << maxTessLevel << std::endl;
+
     // configure global opengl state
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
 
     // build and compile our shader program
     // ------------------------------------
-    Shader heightMapShader("heightmapReader.vs", "heightmapReader.fs");
+    Shader tessHeightMapShader("gpuheight.vs", "gpuheight.fs", nullptr, "gpuheight.tcs", "gpuheight.tes");
 
-    // Load the heightmap from the set image.
+    // load and create a texture
     // -------------------------
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
+    int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true);
-    int imageWidth, imageHeight, nrChannels;
-    unsigned char* heightmapData = stbi_load("HeightMaps/GreatLakeHeightmap.png", &imageWidth, &imageHeight, &nrChannels, 0);
-    if (heightmapData)
+    unsigned char* data = stbi_load("HeightMaps/GreatLakeHeightmap.png", &width, &height, &nrChannels, STBI_rgb_alpha);
+    if (data)
     {
-        std::cout << "Loaded heightmap of size " << imageHeight << " x " << imageWidth << std::endl;
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        
+        tessHeightMapShader.setInt("heightMap", 0);
+        std::cout << "Loaded heightmap of size " << height << " x " << width << std::endl;
     }
     else
     {
         std::cout << "Failed to load texture" << std::endl;
     }
 
+    stbi_image_free(data);
+
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     std::vector<float> vertices;
-    //float yScale = 64.0f / 256.0f, yShift = 16.0f;
-    float yScale = 1024.f / 256.0f, yShift = 8.f;
-    int rez = 1;
-    unsigned bytePerPixel = nrChannels;
-    for (int i = 0; i < imageHeight; i++)
-    {
-        for (int j = 0; j < imageWidth; j++)
-        {
-            unsigned char* pixelOffset = heightmapData + (j + imageWidth * i) * bytePerPixel;
-            unsigned char y = pixelOffset[0];
 
-            // vertex
-            vertices.push_back(-imageHeight / 2.0f + imageHeight * i / (float)imageHeight);   // vx
-            vertices.push_back((int)y * yScale - yShift);   // vy
-            vertices.push_back(-imageWidth / 2.0f + imageWidth * j / (float)imageWidth);   // vz
+    unsigned rez = 40;
+    for (unsigned i = 0; i <= rez - 1; i++)
+    {
+        for (unsigned j = 0; j <= rez - 1; j++)
+        {
+            vertices.push_back(-width / 2.0f + width * i / (float)rez); // v.x
+            vertices.push_back(0.0f); // v.y
+            vertices.push_back(-height / 2.0f + height * j / (float)rez); // v.z
+            vertices.push_back(i / (float)rez); // u
+            vertices.push_back(j / (float)rez); // v
+
+            vertices.push_back(-width / 2.0f + width * (i + 1) / (float)rez); // v.x
+            vertices.push_back(0.0f); // v.y
+            vertices.push_back(-height / 2.0f + height * j / (float)rez); // v.z
+            vertices.push_back((i + 1) / (float)rez); // u
+            vertices.push_back(j / (float)rez); // v
+
+            vertices.push_back(-width / 2.0f + width * i / (float)rez); // v.x
+            vertices.push_back(0.0f); // v.y
+            vertices.push_back(-height / 2.0f + height * (j + 1) / (float)rez); // v.z
+            vertices.push_back(i / (float)rez); // u
+            vertices.push_back((j + 1) / (float)rez); // v
+
+            vertices.push_back(-width / 2.0f + width * (i + 1) / (float)rez); // v.x
+            vertices.push_back(0.0f); // v.y
+            vertices.push_back(-height / 2.0f + height * (j + 1) / (float)rez); // v.z
+            vertices.push_back((i + 1) / (float)rez); // u
+            vertices.push_back((j + 1) / (float)rez); // v
         }
     }
-    std::cout << "Loaded " << vertices.size() / 3 << " vertices" << std::endl;
-    stbi_image_free(heightmapData);
+    std::cout << "Loaded " << rez * rez << " patches of 4 control points each" << std::endl;
+    std::cout << "Processing " << rez * rez * 4 << " vertices in vertex shader" << std::endl;
 
-    std::vector<unsigned> indices;
-    for (unsigned i = 0; i < imageHeight - 1; i += rez)
-    {
-        for (unsigned j = 0; j < imageWidth; j += rez)
-        {
-            for (unsigned k = 0; k < 2; k++)
-            {
-                indices.push_back(j + imageWidth * (i + k * rez));
-            }
-        }
-    }
-    std::cout << "Loaded " << indices.size() << " indices" << std::endl;
-
-    const int numStrips = (imageHeight - 1) / rez;
-    const int numTrisPerStrip = (imageWidth / rez) * 2 - 2;
-    std::cout << "Created lattice of " << numStrips << " strips with " << numTrisPerStrip << " triangles each" << std::endl;
-    std::cout << "Created " << numStrips * numTrisPerStrip << " triangles total" << std::endl;
-
-    // first, configure the cube's VAO (and terrainVBO + terrainIBO)
-    unsigned int terrainVAO, terrainVBO, terrainIBO;
+    // first, configure the cube's VAO (and terrainVBO)
+    unsigned int terrainVAO, terrainVBO;
     glGenVertexArrays(1, &terrainVAO);
     glBindVertexArray(terrainVAO);
 
     glGenBuffers(1, &terrainVBO);
     glBindBuffer(GL_ARRAY_BUFFER, terrainVBO);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.size(), &vertices[0], GL_STATIC_DRAW);
 
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    // texCoord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(sizeof(float) * 3));
+    glEnableVertexAttribArray(1);
 
-    glGenBuffers(1, &terrainIBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, terrainIBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned), &indices[0], GL_STATIC_DRAW);
+    glPatchParameteri(GL_PATCH_VERTICES, NUM_PATCH_PTS);
 
     // render loop
     // -----------
@@ -170,40 +183,31 @@ int main()
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
-        std::cout << deltaTime << "ms (" << 1.0f / deltaTime << " FPS)" << std::endl;
-
-        // Process Input
+        // input
         // -----
         processInput(window);
 
-        // Render
+        // render
         // ------
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // Be sure to activate shader when setting uniforms/drawing objects
-        heightMapShader.use();
+        // be sure to activate shader when setting uniforms/drawing objects
+        tessHeightMapShader.use();
 
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100000.0f);
         glm::mat4 view = camera.GetViewMatrix();
-        heightMapShader.setMat4("projection", projection);
-        heightMapShader.setMat4("view", view);
+        tessHeightMapShader.setMat4("projection", projection);
+        tessHeightMapShader.setMat4("view", view);
 
         // world transformation
         glm::mat4 model = glm::mat4(1.0f);
-        heightMapShader.setMat4("model", model);
+        tessHeightMapShader.setMat4("model", model);
 
-        // render the cube
+        // render the terrain
         glBindVertexArray(terrainVAO);
-        //        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-        for (unsigned strip = 0; strip < numStrips; strip++)
-        {
-            glDrawElements(GL_TRIANGLE_STRIP,   // primitive type
-                numTrisPerStrip + 2,   // number of indices to render
-                GL_UNSIGNED_INT,     // index data type
-                (void*)(sizeof(unsigned) * (numTrisPerStrip + 2) * strip)); // offset to starting index
-        }
+        glDrawArrays(GL_PATCHES, 0, NUM_PATCH_PTS * rez * rez);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -215,7 +219,6 @@ int main()
     // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &terrainVAO);
     glDeleteBuffers(1, &terrainVBO);
-    glDeleteBuffers(1, &terrainIBO);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -230,14 +233,16 @@ void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 
+    constexpr float CAMERA_SPEED = 100.f;
+
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        camera.ProcessKeyboard(FORWARD, deltaTime);
+        camera.ProcessKeyboard(FORWARD, deltaTime * CAMERA_SPEED);
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        camera.ProcessKeyboard(BACKWARD, deltaTime);
+        camera.ProcessKeyboard(BACKWARD, deltaTime * CAMERA_SPEED);
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        camera.ProcessKeyboard(LEFT, deltaTime);
+        camera.ProcessKeyboard(LEFT, deltaTime * CAMERA_SPEED);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        camera.ProcessKeyboard(RIGHT, deltaTime);
+        camera.ProcessKeyboard(RIGHT, deltaTime * CAMERA_SPEED);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
@@ -253,22 +258,15 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 // ---------------------------------------------------------------------------------------------
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int modifiers)
 {
-     if (action == GLFW_PRESS)
+    if (action == GLFW_PRESS)
     {
         switch (key)
         {
-        case GLFW_KEY_SPACE:
-            useWireframe = 1 - useWireframe;
-            break;
-        case GLFW_KEY_G:
-            displayGrayscale = 1 - displayGrayscale;
-            break;
         default:
             break;
         }
     }
 }
-
 
 // glfw: whenever the mouse moves, this callback is called
 // -------------------------------------------------------
